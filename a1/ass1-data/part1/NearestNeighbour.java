813024505d2f59b110cf2b958a412009828d20a2
David Dobbie <david.dobbie@ecs.vuw.ac.nz>
2018-03-16 10:51:16 +1300

    made p1 main file, structure

---
+++ a1/ass1-data/part1/NearestNeighbour.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Implementation of the k-Nearest Neighbour algorithm. Used to classify the iris data set given.
+ * COMP307 Assignment 1 - Part 1
+ * David Dobbie - 300340161
+ */
+public class NearestNeighbour {
+
+
+
+    public NearestNeighbour(){
+        
+    }
+
+
+
+
+
+}
+
+public static void main(String [ ] args)
+{
+      NearestNeighbour nn = new NearestNeighbour();
+      
+}
