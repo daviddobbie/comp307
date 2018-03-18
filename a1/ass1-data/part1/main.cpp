adbfe8908fd9fbe4565e32ed402173b307e7b808
David Dobbie <david.dobbie@ecs.vuw.ac.nz>
2018-03-19 10:48:18 +1300

    fixed argument iteration

--- a1/ass1-data/part1/main.cpp
+++ a1/ass1-data/part1/main.cpp
@@ -1,15 +1,31 @@
-// Name of program mainreturn.cpp
+/*
+Name: David Dobbie
+
+Answer for COMP307 Assignment 1 - Part 1
+Nearest Neighbour Method
+*/
 
 #include <iostream>
 using namespace std;
 
+
+/*
+@Inputs: file name to be parse
+@Function: parses the data provided in the file into the format required
+            to apply the ML technqiue
+*/
+
 int parseFile(char* fileName){
-  cout << "Parsing..."<< fileName;
+  cout << "Parsing..."<< fileName << "\n";
 
   return 0;
 
 }
 
+/*
+@Inputs: command arguments: the dataset to be parse and read from
+@Function: to initialise the datasets in the program to be processed
+*/
 
 int main(int argc, char** argv)
 {
@@ -20,8 +36,10 @@ int main(int argc, char** argv)
   }
   else{
     trainingFile = *(argv+1);
-    testFile = *(argv+1);
     parseFile(trainingFile);
+
+    testFile = *(argv+2);
+    parseFile(testFile);
   }
 
   return 0;
