8a77eb3e2ba343f5ad6e2ea709703a890f7b85a7
David Dobbie <david.dobbie@ecs.vuw.ac.nz>
2018-03-26 14:37:44 +1300

    Started DT method. Started mean method.

---
+++ a1/ass1-data/part2/main.cpp
@@ -0,0 +1,46 @@
+/*
+Name: David Dobbie
+
+Answer for COMP307 - Introduction to Artifical Intelligence
+Assignment 1 - Part 2
+Decision Tree Learning Method
+*/
+
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+bool DEBUG = false;
+
+
+
+
+
+
+
+/*
+@Inputs: command arguments: the dataset to be parse and read from
+@Function: to initialise the datasets in the program to be processed
+*/
+int main(int argc, char** argv)
+{
+
+    if(argc != 3){
+        std::cerr << "You must enter two datasets, the training set and the test set\n";
+    }
+    return 1;
+
+
+    
+}
