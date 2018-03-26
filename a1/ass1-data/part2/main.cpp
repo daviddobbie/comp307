2821863dec7b49baabe9cad9a92f4cf23aaa617b
David Dobbie <david.dobbie@ecs.vuw.ac.nz>
2018-03-26 16:10:41 +1300

    Added parsing for the first line of data

--- a1/ass1-data/part2/main.cpp
+++ a1/ass1-data/part2/main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <sstream>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,10 +40,10 @@ class Instance{
     Inputs: category type, ordered vector of booleans
     Function: initalises the 
     */
-    Instance(int cat, vector<bool> v)
+    Instance(int i, int cat, vector<bool> v)
     {
-        if(DEBUG) cout << "Created a new instance\n";
-        id +=1;
+        //if(DEBUG) cout << "Created a new instance\n";
+        id =i;
         category =cat;
         vals = v;
 
@@ -56,6 +57,61 @@ class Instance{
 
 };
 
+/*
+@Inputs: file name to be parse
+@Function: parses the data provided in the file into the format required
+            to apply the ML technqiue
+*/
+ vector<Instance> parseFile(char* fileName, int isTraining){
+  char* localName = fileName;
+  char line[256];
+  int iterId=0;
+  int catRead;
+
+  bool isFirstLine = true;
+
+  vector<bool> valsRead;
+
+  vector<Instance> vInst;
+  vector<string> catNames;
+  vector<string> attNames;
+
+
+  std::cout << "Parsing..."<< localName<< "\n";
+  FILE * inFile;
+  inFile = fopen(localName, "r");
+  if(!inFile){
+    std::cout << "Opening file failed\n";
+    return vInst;
+  }
+
+  // parse thru each line, returns it as a string
+  while (fgets(line, sizeof(line), inFile)) { // standard C I/O file reading loop
+      if(DEBUG) printf("%s",line);
+      
+      
+      stringstream lineStr(line); 
+      string word;
+      
+      while(lineStr >> word){
+        if(DEBUG)cout<<"Parsed Word: "<<word<<"\n";
+        if(isFirstLine) attNames.push_back(word);
+
+      }
+
+      if(isFirstLine) isFirstLine=false; //finishes reading first line
+
+      Instance inst(iterId, catRead, valsRead);
+      //diagnostic print out of data stored
+      vInst.push_back(inst);
+  }
+
+  fclose(inFile);
+  std::cout << "Parsing "<< localName<< " Complete \n";
+  return vInst;
+
+}
+
 
 /*
 @Inputs: command arguments: the dataset to be parse and read from
@@ -63,12 +119,16 @@ class Instance{
 */
 int main(int argc, char** argv)
 {
+    char * trainingFile;
+    char * testFile;
+
     int numCategories;
     int numAtts;
 
     vector<string> categoryNames;
     vector<string> attNames;
-    vector<Instance> allInstances;
+    vector<Instance> trainingInstances;
+    vector<Instance> testInstances;
 
     /*
     vector<bool> dataVect;
@@ -79,9 +139,15 @@ int main(int argc, char** argv)
 
     if(argc != 3){
         std::cerr << "You must enter two datasets, the training set and the test set\n";
+        return 1;
     }
-    return 1;
 
+    trainingFile = *(argv+1);
+    trainingInstances = parseFile(trainingFile, 1);
+
+    testFile = *(argv+2);
+    testInstances =  parseFile(testFile, 0);
+    return 0;
 
     
 }
