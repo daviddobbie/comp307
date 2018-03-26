6f8eb2a70f13635bb0340cd554d6da774bb9fee0
David Dobbie <david.dobbie@ecs.vuw.ac.nz>
2018-03-26 15:13:23 +1300

    Added instance class for DT leaner.

--- a1/ass1-data/part2/main.cpp
+++ a1/ass1-data/part2/main.cpp
@@ -21,12 +21,40 @@ using namespace std;
 #include <string.h>
 #include <math.h>
 
-bool DEBUG = false;
+bool DEBUG = true;
 
 
 
+/*
+    The class object of an instance
+*/
+class Instance{
+    private:
+    int id = 0;
+    int category;
+    vector<bool> vals;
 
+    public:
+    /*
+    Inputs: category type, ordered vector of booleans
+    Function: initalises the 
+    */
+    Instance(int cat, vector<bool> v)
+    {
+        if(DEBUG) cout << "Created a new instance\n";
+        id +=1;
+        category =cat;
+        vals = v;
 
+    }
+    bool getAtt(int index){
+        return vals[index];
+    }
+    int getCategory(){
+        return category;
+    }
+
+};
 
 
 /*
@@ -35,6 +63,19 @@ bool DEBUG = false;
 */
 int main(int argc, char** argv)
 {
+    int numCategories;
+    int numAtts;
+
+    vector<string> categoryNames;
+    vector<string> attNames;
+    vector<Instance> allInstances;
+
+    /*
+    vector<bool> dataVect;
+    dataVect.push_back(true);
+    Instance newInstance(2, dataVect);
+    */
+
 
     if(argc != 3){
         std::cerr << "You must enter two datasets, the training set and the test set\n";
