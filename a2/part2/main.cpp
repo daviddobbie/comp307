74814d9ea8306f63610f7f1f569b86a28ecf3d65
David Dobbie <david.dobbie@ecs.vuw.ac.nz>
2018-04-23 10:03:01 +1200

    Added main file for  a2p2

---
+++ a2/part2/main.cpp
@@ -0,0 +1,92 @@
+/*
+Name: David Dobbie
+
+Answer for COMP307 - Introduction to Artifical Intelligence
+Assignment 2 - Part 2
+Genetic Programming Symbolic Regression
+*/
+
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+bool DEBUG = false;
+
+
+/*
+@Inputs: file name to be parse
+@Function: parses the data provided in the file into the format required
+            to apply the ML technqiue
+*/
+ void parseFile(char* fileName, int isTraining){
+  char* localName = fileName;
+
+  char line[256];
+
+  int iterId=0;
+
+
+
+  std::cout << "Parsing..."<< localName<< "\n";
+
+  FILE * inFile;
+  inFile = fopen(localName, "r");
+
+  if(!inFile){
+
+    std::cout << "Opening file failed\n";
+    return;
+  }
+
+  // parse thru each line, returns it as a string
+  while (fgets(line, sizeof(line), inFile)) { // standard C I/O file reading loop
+  }
+
+  fclose(inFile);
+  std::cout << "Parsing "<< localName<< " Complete \n";
+  return;
+
+}
+
+/*
+@Inputs: command arguments: the dataset to be parse and read from
+@Function: to initialise the datasets in the program to be processed
+*/
+int main(int argc, char** argv)
+{
+  char * trainingFile;
+  char * testFile;
+
+  int k;
+
+
+
+  if(argc != 4){
+    std::cerr << "You must enter two datasets, the training set and the test set, and a k value\n";
+  }
+  else{
+    trainingFile = *(argv+1);
+    parseFile(trainingFile, 1);
+
+    testFile = *(argv+2);
+    parseFile(testFile, 0);
+
+
+    parseFile(testFile, 1);
+  std::cout << "Closing...\n";
+  return 0;
+}
+
+
+
